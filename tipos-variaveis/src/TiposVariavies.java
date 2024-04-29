cc1430529ae818a4c68f8e9e61c27dde1476c14c
David <davidsoares585@gmail.com>
2024-04-29 23:04:39 +0100

    Tipos de variáveis mais usadas no java

--- tipos-variaveis/src/TiposVariavies.java
+++ tipos-variaveis/src/TiposVariavies.java
@@ -1,6 +1,29 @@
+import java.util.Date;
+
 public class TiposVariavies {
     public static void main(String[] args) throws Exception {
     
-        
+        // O tipo char representa apenas um caracter
+        char sexo = 'M';
+
+        //A classe String represnta a uma cadeia de caracteres
+        String nome = "Fulano de Tal";
+
+        //O int representa números inteiros
+        int idade = 17;
+
+        //O short representa números interios pesquenos (max: 127)
+        short numeroCurto = 1;
+
+        //Double represneta números decimais
+        double peso = 72.6;
+
+        //Boolean representa um valor verdadeiro ou falso
+        Boolean maiorDeIdade = false;
+
+        //O obejto Date é usado para representar datas  
+        Date dataNascimento = new Date();
+
+
     }
 }
